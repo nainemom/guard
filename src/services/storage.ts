596c028eec1619dca8165af250c346fb28272c73
Amir Momenian <nainemom@gmail.com>
2022-10-19 04:01:13 +0330

    chore: make storage type safe

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,5 +1,6 @@
 import { STORAGE_PREFIX } from '@/constants';
 import { createEventTarget } from '@/utils/events';
+import { StateUpdater, useCallback, useEffect, useState } from 'preact/hooks';
 
 const eventTarget = createEventTarget();
 export const on = eventTarget.on;
@@ -17,9 +18,30 @@ export const get = (key: string) => {
   }
 };
 
-export const set = (key: string, newValue: any) => {
+export const set = (key: string, newValue: any, emit: boolean = true) => {
   const newRawValue = JSON.stringify(newValue);
   localStorage.setItem(`${STORAGE_PREFIX}${key}`, newRawValue);
   eventTarget.emit(`update:${key}`, newValue);
   return newRawValue;
 };
+
+export const useStorage = <T>(key: string): [T, StateUpdater<T>] => {
+  const [value, setValue] = useState<T>(get(key));
+
+  const handleStorageUpdate = useCallback((event: CustomEventInit) => {
+    setValue(event.detail);
+  }, [setValue]);
+
+  useEffect(() => {
+    on(`update:${key}`, handleStorageUpdate);
+    return () => {
+      off(`update:${key}`, handleStorageUpdate);
+    };
+  }, [setValue]);
+
+  useEffect(() => {
+    set(key, value);
+  }, [value]);
+
+  return [value, setValue];
+}
