fc4d26f3f3527b2eb0cef062827e1410fe151c86
Amir Momenian <nainemom@gmail.com>
2022-10-26 00:35:48 +0330

    fix: sticky button padding

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -13,7 +13,7 @@ export default function Body({ children, stickyArea, stickyPadding, className }:
     <div className="flex-grow overflow-hidden relative contain-layout">
       <div className={cx('overflow-auto h-full', stickyArea && stickyPadding && 'pb-12', className)}>{ children }</div>
       { stickyArea && (
-        <div className="fixed w-full left-0 bottom-0 p-4 h-auto flex flex-row justify-end pointer-events-none">
+        <div className="fixed w-full left-0 bottom-0 p-3 h-auto flex flex-row justify-end pointer-events-none">
           <div className="pointer-events-auto">
             { stickyArea }
           </div>
