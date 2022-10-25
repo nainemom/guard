31cbf40bba77fdc4a32b83b1f4a9692af9592da8
Amir Momenian <nainemom@gmail.com>
2022-10-25 20:11:03 +0330

    feat: refactor tabs style

--- src/components/layout/Tabs.tsx
+++ src/components/layout/Tabs.tsx
@@ -6,16 +6,13 @@ export default function Tabs() {
   return (
     <nav class="x-tabs-container">
       <Link class="x-button x-button-md x-tab-button" activeClassName="x-tab-button-active" href="/profile">
-        <Icon name="manage_accounts" className="w-6 h-6" />
-        Profile
+        <Icon name="manage_accounts" className="w-7 h-7" />
       </Link>
       <Link class="x-button x-button-md x-tab-button gap-1" activeClassName="x-tab-button-active" href="/encrypt">
-        <Icon name="mail_lock" className="w-6 h-6" />
-        Encrypt
+        <Icon name="mail_lock" className="w-7 h-7" />
       </Link>
       <Link class="x-button x-button-md x-tab-button gap-1" activeClassName="x-tab-button-active" href="/decrypt">
-        <Icon name="drafts" className="w-6 h-6" />
-        Decrypt
+        <Icon name="drafts" className="w-7 h-7" />
       </Link>
     </nav>
   );
