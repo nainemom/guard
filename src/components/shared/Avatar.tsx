62b35d8980fd9d740b29a443c59a3193bb564218
Amir Momenian <nainemom@gmail.com>
2022-10-26 01:23:36 +0330

    fix: avatar component props typo

--- src/components/shared/Avatar.tsx
+++ src/components/shared/Avatar.tsx
@@ -2,12 +2,12 @@ import { CryptographyPublicKey, hash } from "@/services/cryptography";
 import { cx } from "@/utils/cx";
 import { useEffect, useMemo, useState } from "preact/hooks";
 
-export type IconProps = {
+export type AvatarProps = {
   publicKey: CryptographyPublicKey,
   className?: string,
 }
 
-export default function Avatar({ publicKey, className }: IconProps) {
+export default function Avatar({ publicKey, className }: AvatarProps) {
   const [slug, setSlug] = useState<string>('');
   useEffect(() => {
     hash(publicKey).then(setSlug);
