b810ad2416128757d87393887b1dca7f6ce0fcd1
Amir Momenian <nainemom@gmail.com>
2022-10-27 20:07:36 +0330

    fix: logo path

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -16,7 +16,7 @@ export default function Header({ title, subtitle, startButtons, endButtons }: He
         </div>
       ) }
       <div class="flex-grow flex flex-row items-center gap-3">
-        <img src="/logo_white.svg" width="48" height="48" className="w-10 h-10" />
+        <img src={`${import.meta.env.BASE_URL}logo_white.svg`} width="48" height="48" className="w-10 h-10" />
         <div>
           { title && (<h2 class="text-xl font-semibold">{ title }</h2>) }
           { subtitle && (<p className="text-xs text-section-subtitle">{subtitle}</p>) }
