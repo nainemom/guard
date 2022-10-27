e574c1ad87905fa598207862bc0c2204c315c02a
Amir Momenian <nainemom@gmail.com>
2022-10-27 19:52:40 +0330

    feat: update brand

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,8 +15,12 @@ export default function Header({ title, subtitle, startButtons, endButtons }: He
           { startButtons }
         </div>
       ) }
-      <div class="flex-grow">
-        { title && (<h2 class="text-2xl font-semibold">{ title }</h2>) }
+      <div class="flex-grow flex flex-row items-center gap-3">
+        <img src="/logo_white.svg" width="48" height="48" className="w-10 h-10" />
+        <div>
+          { title && (<h2 class="text-xl font-semibold">{ title }</h2>) }
+          { subtitle && (<p className="text-xs text-section-subtitle">{subtitle}</p>) }
+        </div>
       </div>
       { endButtons && (
         <div>
